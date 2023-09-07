86e2bb03c781f03982e931ec6ec0b4509d7ebeaf
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-09-08 02:22:02 +0530

    post route for saving books data in database done

--- server/app.js
+++ server/app.js
@@ -5,7 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const mongoose = require("mongoose");
 
+const Book = require("./models/Book");
+
 const app = express();
+app.use(express.json());
 
 mongoose
     .connect(process.env.DB_URL)
@@ -19,6 +22,26 @@ mongoose
 app.get("/", (req, res) => {
     // console.log(req);
     return res.status(234).send("Hello World");
+});
+
+app.post("/books", async (req, res) => {
+    try {
+        const { title, author, publishYear, description } = req.body;
+        if (!title || !author || !publishYear || !description) {
+            return res.status(400).send({ message: "Some fields missing" });
+        }
+
+        const book = await Book.create({
+            title: title,
+            author: author,
+            publishYear: publishYear,
+            description: description,
+        });
+        res.status(201).send(book);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({ message: "Internal Server Error" });
+    }
 });
 
 let server_port = process.env.PORT || 4001;
