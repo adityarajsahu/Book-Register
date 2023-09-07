0223946942eecc11e0b7e43211439b51f47f6a1d
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-09-08 01:56:30 +0530

    mongodb database setup

--- server/app.js
+++ server/app.js
@@ -3,7 +3,23 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.get("/", (req, res) => {
+    // console.log(req);
+    return res.status(234).send("Hello World");
+});
 
 let server_port = process.env.PORT || 4001;
 app.listen(server_port, () => {
