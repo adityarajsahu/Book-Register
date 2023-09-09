4e49803a62e4ec4d921fabe3e721b29eaeb1e794
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-09-10 00:12:21 +0530

    routes created in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import CreateBook from "./pages/CreateBook";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBook";
+import Home from "./pages/Home";
+import ShowBook from "./pages/ShowBook";
+
 import "./App.css";
 
 function App() {
-    return <div className="App">Hello World!</div>;
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books/create" element={<CreateBook />} />
+            <Route path="/books/show/:id" element={<ShowBook />} />
+            <Route path="/books/edit/:id" element={<EditBook />} />
+            <Route path="/books/delete/:id" element={<DeleteBook />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ client/src/pages/ShowBook.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowBook = () => {
+    return <div>Show Book</div>;
+};
+
+export default ShowBook;
