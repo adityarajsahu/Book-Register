9a77e4c8560d168b393bcfbfcc384a05dd6f7c9b
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-09-10 09:46:14 +0530

    AppBar created

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Box, Typography, Toolbar, AppBar, Button, IconButton } from "@mui/material";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import InfoIcon from "@mui/icons-material/Info";
+import App from "../App";
 
 const Home = () => {
-    return <div>Home</div>;
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get("http://localhost:5500/books")
+            .then((res) => {
+                setBooks(res.data.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                        Book Register
+                    </Typography>
+                    <Button color="inherit" component={Link} to="/books/create">
+                        <AddBoxOutlinedIcon />
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 };
 
 export default Home;
